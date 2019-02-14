aa6fd9cc834d8b958acef4829c514f2ef7d98994
Imandra McKenzie and Lex Wilkins <lex@>
2019-02-13 19:49:29 -0800

    Add subtract, multiply, and divide form fields and submit buttons that print results on the page.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,66 @@
 $(document).ready(function(){
   $("#add-value").hide();
+  $("#sub-value").hide();
+  $("#mult-value").hide();
+  $("#div-value").hide();
+
+
   $(".add-form form").submit(function(event){
-    var numOneInput = parseInt($("input#num1").val());
-    var numTwoInput = parseInt($("input#num2").val());
+    var numOneInput = parseInt($("input#addNum1").val());
+    var numTwoInput = parseInt($("input#addNum2").val());
 
     var addedNums = function(numberOne, numberTwo) {
       return numberOne + numberTwo;
     };
 
-    $(".add-total").text(addedNums(numOneInput, numTwoInput));
+    $("#add-total").text(addedNums(numOneInput, numTwoInput));
 
     $("#add-value").show();
+
+    event.preventDefault();
+  });
+
+  $(".sub-form form").submit(function(event){
+    var numOneInput = parseInt($("input#subNum1").val());
+    var numTwoInput = parseInt($("input#subNum2").val());
+
+    var subbedNums = function(numberOne, numberTwo) {
+      return numberOne - numberTwo;
+    };
+
+    $("#sub-total").text(subbedNums(numOneInput, numTwoInput));
+
+    $("#sub-value").show();
+
+    event.preventDefault();
+  });
+
+  $(".mult-form form").submit(function(event){
+    var numOneInput = parseInt($("input#multnum1").val());
+    var numTwoInput = parseInt($("input#multnum2").val());
+
+    var multNums = function(numberOne, numberTwo) {
+      return numberOne * numberTwo;
+    };
+
+    $("#mult-total").text(multNums(numOneInput, numTwoInput));
+
+    $("#mult-value").show();
+
+    event.preventDefault();
+  });
+
+  $(".divi-form form").submit(function(event){
+    var numOneInput = parseInt($("input#divNum1").val());
+    var numTwoInput = parseInt($("input#divNum2").val());
+
+    var divNums = function(numberOne, numberTwo) {
+      return numberOne / numberTwo;
+    };
+
+    $("#div-total").text(divNums(numOneInput, numTwoInput));
+
+    $("#div-value").show();
 
     event.preventDefault();
   });
